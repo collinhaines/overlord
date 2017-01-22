6a5e3738f6efb1633de030322d5d586a26532abc
Collin Haines <collinhaines@me.com>
2017-01-21 19:28:01 -0500

    Added Server Maintenance Script

---
+++ maintenance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Author:  Collin Haines
+# Updated: Nov 18, 2016
+
+sudo apt-get -y update;
+sudo apt-get -y upgrade;
+sudo apt-get -y dist-upgrade;
+sudo apt-get -y autoremove;
+sudo apt-get -y autoclean;
